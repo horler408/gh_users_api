4931a8eafee44de443a0f08144ef444ada290f52
horler408 <horler.web@gmail.com>
2020-10-25 20:47:57 +0100

    initial API call

--- js/app.js
+++ js/app.js
@@ -1,10 +1,30 @@
+//DOM Manipulation
+const container = document.querySelector('.user_container');
+const showBtn = document.querySelector('.show-btn')
+
 const baseUrl = `https://api.github.com/users`
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
 
 const getRequest = () => {
 	fetch(baseUrl)
 	.then(response => response.json())
-	.then(data => {
-		console.log(data)
+	.then(users => {
+		console.log(users)
+
+		users.map(user => {
+			showBtn.addEventListener('click', () => {
+				const ul = document.createElement('ul')
+				const li = document.createElement('li')
+
+				li.innerHTML = user.login
+
+				append(ul, li)
+				append(container, ul)
+			})
+		})
 	})
 }
 
